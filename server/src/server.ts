47511d44c5b521383c2571b7c9c5750ccd83361d
kaneganteng <kaneesasta@gmail.com>
2024-11-25 12:55:45 -0500

    changed assert ot with

--- server/src/server.ts
+++ server/src/server.ts
@@ -12,13 +12,13 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
-if (process.env.NODE_ENV === 'production') {
+// if (process.env.NODE_ENV === 'production') {
   app.use(express.static('../client/dist'));
 
    app.get('*', (_req, res) => {
     res.sendFile('../client/dist/index.html');
   });
-}
+// }
 
 app.listen(PORT, () => {
   console.log(`API server running on port ${PORT}!`);
